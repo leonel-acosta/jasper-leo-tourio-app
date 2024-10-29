225b4021f0f1f1c7f7c41f26b2c13999eabae0a5
leonel-acosta <leoacosta88@gmail.com>
2024-10-29 16:42:02 +0100

    add create - POST method to form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35d43ada40d62666"; }
+/******/ 		__webpack_require__.h = function() { return "aa9062f4078fc734"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
